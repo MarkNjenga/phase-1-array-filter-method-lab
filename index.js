7f2d0d7b5022b19a2daf3ce900c21e427d9ec024
Mark Njenga <marknjenga25@gmail.com>
2024-07-08 14:00:27 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    const lowercaseQuery = query.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+
+        const lowercaseDriver = driver.toLowerCase();
+        return lowercaseDriver.includes(lowercaseQuery);
+    });
+
+    return matchingDrivers;
+}
+function fuzzyMatch(drivers, query){
+    const lowercaseQuery = query.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+        const lowercaseDriver = driver.toLowerCase();
+        return lowercaseDriver.startsWith(lowercaseQuery);
+    }); 
+
+    return matchingDrivers;
+}
+function matchName(drivers, query) {
+    const matchingDrivers = drivers.filter(driver => {
+        return driver.name.toLowerCase() === query.toLowerCase();
+    });
+
+    return matchingDrivers;
+}
